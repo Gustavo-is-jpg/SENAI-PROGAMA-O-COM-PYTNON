6e2104dbca6dbafe066a938add505acf6e227e56
Gustavo Gomes Gonçalvez <gugomesjv@gmail.com>
2025-05-10 11:18:34 -0300

    Add files via upload

---
+++ Atividade2.py
@@ -0,0 +1,65 @@
+
+# Conversão de Matros para Centimetros
+
+medida = float(input("Digite a medida em metros: "))
+
+centimetros = medida * 100
+print(f"A medida em centimetros é: {centimetros} cm")
+
+
+# Ária de um círculo
+
+raio = float(input("Digite o valor da raio: "))
+
+calcule = (3.14159 * (raio ** 2))
+print(f'A área do cerculo é {calcule} ')
+
+
+# Área do quadrado e o dobro
+
+lado = float(input('Digite o valor do lado do quadrado: '))
+
+print('O drobo da ária é:', lado * lado * 2)
+
+
+
+
+# Cálculo de Salário
+
+ganho_por_hora = float(input("Digite o salário por hora: "))
+numeros_de_horas_por_mes = float(input("Digite o número de horas trabalhadas no mês: "))
+
+print(f'O total do salario no mês é {ganho_por_hora * numeros_de_horas_por_mes} R$')
+
+
+
+
+
+# Conversão de Fahrenheit para Celsius
+
+temperatura_fahrenhait = float(input("Digite a temperatura em Fahrenheit: "))
+
+celsius = 5 * ((temperatura_fahrenhait - 32) / 9)
+
+print(f'A temperatura em Celsius é: {celsius} °C')
+
+
+
+
+
+# Conversão de Celsius para Fahrenheit
+
+
+temperatua = float(input("Digite a temperatura em Celsius: "))
+
+fahrenheit = (temperatua * 9 / 5) + 32
+
+
+
+
+# Conversão de Gigabytes para Megabytes
+
+gigabytes = float(input("Digite o valor em Gigabytes: "))
+
+megabytes = gigabytes * 1024
+print(f"O valor em Megabytes é: {megabytes} MB")
